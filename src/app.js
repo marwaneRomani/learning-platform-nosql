5c6bb57260564bdc0373cb0099352c5deff49066
marwane <marwanromani2018@gmail.com>
2025-01-08 13:30:20 +0100

    organisation du point d'entrée de l'application

--- src/app.js
+++ src/app.js
@@ -12,10 +12,22 @@ const app = express();
 
 async function startServer() {
   try {
-    // TODO: Initialiser les connexions aux bases de données
-    // TODO: Configurer les middlewares Express
-    // TODO: Monter les routes
-    // TODO: Démarrer le serveur
+    // Initialiser les connexions aux bases de données
+    await db.connectMongo();
+    await db.connectRedis();
+
+    // Configurer les middlewares Express
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    // Monter les routes
+    app.use('/api/courses', courseRoutes);
+    app.use('/api/students', studentRoutes);
+
+    // Démarrer le serveur
+    app.listen(config.port, () => {
+      console.log(`running on port ${config.port}`);
+    });
   } catch (error) {
     console.error('Failed to start server:', error);
     process.exit(1);
@@ -24,7 +36,10 @@ async function startServer() {
 
 // Gestion propre de l'arrêt
 process.on('SIGTERM', async () => {
-  // TODO: Implémenter la fermeture propre des connexions
+  console.log("Closing connections...");
+  db.closeConnections();
+  process.exit(0);
+
 });
 
 startServer();
